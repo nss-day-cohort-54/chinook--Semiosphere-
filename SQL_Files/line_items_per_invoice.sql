f245ca293b27feddd8455057780f8cd1fec4ade2
Zach Dugger <ztd522@gmail.com>
2022-05-25 11:32:16 -0400

    exercise 11

---
+++ SQL_Files/line_items_per_invoice.sql
@@ -0,0 +1,7 @@
+line_items_per_invoice.sql
+
+Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice
+
+SELECT InvoiceId, COUNT(*)
+FROM InvoiceLine
+GROUP BY InvoiceId
