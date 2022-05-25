bd1283ae189c876d8687e3fe423779ffb531370b
Zach Dugger <ztd522@gmail.com>
2022-05-25 11:08:56 -0400

    exercise 8

---
+++ SQL_Files/total_invoices_{year}.sql
@@ -0,0 +1,7 @@
+total_invoices_{year}.sql
+
+How many Invoices were there in 2009 and 2011?
+
+SELECT COUNT(InvoiceDate)
+FROM Invoice
+WHERE strftime('%Y', InvoiceDate) = '2009' OR strftime('%Y', InvoiceDate) = '2011'
